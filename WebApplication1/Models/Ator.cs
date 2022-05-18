cb79c89b25ce8663ff086375511062c97743728c
WESLEYSON PEREIRA ROCHA <wesleyson.procha@senacsp.edu.br>
2022-05-17 21:24:57 -0300

    mensagem

---
+++ WebApplication1/Models/Ator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class Ator: IEntidade
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime DataAlteracao { get; set; }
+
+        public string Nome { get; set; }
+        public string Bio { get; set; }
+        public string FotoPerfilURL { get; set; }
+       
+    }
+    }
+
---
+++ WebApplication1/Models/Entidade.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public interface IEntidade
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime DataAlteracao { get; set; }
+        
+    }
+}
